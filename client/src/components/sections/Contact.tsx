62168f57bf24c492aed172770eb471525c6104fd
Gabriel Marcano <marcanogc@gmail.com>
2026-01-17 14:54:53 -0300

    Update Project

--- client/src/components/sections/Contact.tsx
+++ client/src/components/sections/Contact.tsx
@@ -208,11 +208,11 @@ export const Contact = () => {
               </div>
               
               <img 
-                src="https://cdn.pixabay.com/photo/2018/01/06/07/53/social-3064515_1280.jpg" 
-                alt="Developer workspace illustration" 
+                src="https://www.thebusinesstoolkit.com/wp-content/uploads/2023/04/social-media-blocks.jpeg" 
+                alt="Media Social" 
                 width={300}
                 height={200}
-                className="w-full h-auto rounded-lg mt-6" 
+                className="w-[300px] h-[200px] rounded-lg mt-6" 
               />
             </div>
           </div>
